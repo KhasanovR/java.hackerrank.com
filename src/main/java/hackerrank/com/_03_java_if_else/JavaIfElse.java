f3e4f2d2d8e6d65f95f2fbf92ee51ee6ee1b9e99
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2023-02-03 16:05:31 +0500

    Add solution to task 03

--- src/main/java/hackerrank/com/_03_java_if_else/JavaIfElse.java
+++ src/main/java/hackerrank/com/_03_java_if_else/JavaIfElse.java
@@ -7,7 +7,10 @@ public class JavaIfElse {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
+        if (N % 2 == 0 && ((2 <= N && N <= 5) || N > 20)) {
+            System.out.print("Not ");
+        }
+        System.out.println("Weird");
         scanner.close();
     }
 }
