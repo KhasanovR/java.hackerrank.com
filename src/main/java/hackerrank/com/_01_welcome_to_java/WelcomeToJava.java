39081a6ac486312ec81573a897864e5fd43f92e2
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2023-02-03 16:05:02 +0500

    Add solution to task 01

--- src/main/java/hackerrank/com/_01_welcome_to_java/WelcomeToJava.java
+++ src/main/java/hackerrank/com/_01_welcome_to_java/WelcomeToJava.java
@@ -2,6 +2,7 @@ package hackerrank.com._01_welcome_to_java;
 
 public class WelcomeToJava {
     public static void main(String[] args) {
-
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
