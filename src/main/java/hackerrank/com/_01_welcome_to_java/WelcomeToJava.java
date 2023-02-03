a64201275bd26fe5d51a32258b0662c4510bac89
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2023-02-03 16:35:56 +0500

    Add task 01 with initial state with tests

---
+++ src/main/java/hackerrank/com/_01_welcome_to_java/WelcomeToJava.java
@@ -0,0 +1,7 @@
+package hackerrank.com._01_welcome_to_java;
+
+public class WelcomeToJava {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/hackerrank/com/_01_welcome_to_java/WelcomeToJavaTest.java
@@ -0,0 +1,30 @@
+package hackerrank.com._01_welcome_to_java;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class WelcomeToJavaTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void testCase0() {
+        WelcomeToJava.main(null);
+        Assertions.assertEquals("Hello, World.\n" + "Hello, Java.", outputStreamCaptor.toString().trim());
+    }
+}
